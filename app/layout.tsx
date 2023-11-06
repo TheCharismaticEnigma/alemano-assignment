9668ce4792eeccbdc48025afa800a4fc28987e01
TheCharismaticEnigma <avishkar2002@mail.com>
2023-11-06 12:18:21 +0530

    Enhancement: Add a custom scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
-import '@radix-ui/themes/styles.css';
 import ChakraProvider from '@/providers/ChakraProvider';
 import { Box } from '@chakra-ui/react';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,8 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ChakraProvider>
-          <main className="p-5">
-            <Box className="mx-auto max-w-7xl h-32 ">{children}</Box>
+          <main className="p-3">
+            <Box className="mx-auto max-w-7xl min-h-screen">{children}</Box>
           </main>
         </ChakraProvider>
       </body>
