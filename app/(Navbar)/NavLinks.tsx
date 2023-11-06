866bfd95631454db97663340b323705ec0607a80
TheCharismaticEnigma <avishkar2002@mail.com>
2023-11-06 18:05:38 +0530

    Refactor: Enhance the active & hovered links

--- app/(Navbar)/NavLinks.tsx
+++ app/(Navbar)/NavLinks.tsx
@@ -1,19 +1,51 @@
-import { Tab, TabList, Tabs } from '@chakra-ui/react';
+'use client';
+import { Box, Flex, Tab, TabList, Tabs } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/next-js';
+import { navLinks } from '@/utils/NavLinks';
+import { usePathname } from 'next/navigation';
 
 const NavLinks = () => {
+  const path = usePathname();
+
   return (
-    <Tabs
-      variant={'soft-rounded'}
-      colorScheme="purple"
-      fontWeight={'medium'}
-      className="px-4 text-black"
-    >
-      <TabList gap={'3'}>
-        <Tab color={'black'}>Dashboard</Tab>
-        <Tab color={'black'}>All Courses</Tab>
-        <Tab color={'black'}>New Course</Tab>
-      </TabList>
-    </Tabs>
+    <Flex gap={'2'} px={'4'}>
+      {navLinks.map(({ label, href }) => {
+        //   If it is active link then return a separate styled link
+        if (href === path) {
+          return (
+            <Link
+              key={label}
+              href={href}
+              borderRadius={'3xl'}
+              color={'black'}
+              fontWeight={'medium'}
+              style={{
+                textDecoration: 'none',
+              }}
+              className="px-4 py-2 shadow-sm shadow-indigo-500 bg-gradient-to-r from-purple-300 to-indigo-300 "
+            >
+              {label}
+            </Link>
+          );
+        }
+
+        return (
+          <Link
+            key={label}
+            href={href}
+            borderRadius={'3xl'}
+            color={'black'}
+            fontWeight={'medium'}
+            style={{
+              textDecoration: 'none',
+            }}
+            className="px-4 py-2 hover:shadow-sm hover:shadow-indigo-500 hover:bg-gradient-to-r from-purple-300 to-indigo-300 "
+          >
+            {label}
+          </Link>
+        );
+      })}
+    </Flex>
   );
 };
 
---
+++ utils/NavLinks.ts
@@ -0,0 +1,14 @@
+export const navLinks: NavLinks[] = [
+  {
+    href: '/',
+    label: 'Dashboard',
+  },
+  {
+    href: '/courses',
+    label: 'All Courses',
+  },
+  {
+    href: '/courses/new',
+    label: 'New Course',
+  },
+];
