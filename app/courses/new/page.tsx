79472d6cc5cc3b64cdbf0a44d78ef913f8d3a52c
TheCharismaticEnigma <avishkar2002@mail.com>
2023-11-07 20:37:15 +0530

    Optimize: Dynamically Lazy Load the Client Component

--- app/courses/new/page.tsx
+++ app/courses/new/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import NewCourseForm from '../(components)/NewCourseForm';
+import PageSkeleton from '@/skeletons/PageSkeleton';
+import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+
+const NewCourseForm = dynamic(() => import('../(components)/NewCourseForm'), {
+  ssr: false,
+  loading: () => <PageSkeleton />,
+});
 
 const NewCoursePage = () => {
   return <NewCourseForm />;
 };
 
 export default NewCoursePage;
+
+export const metadata: Metadata = {
+  title: 'The Assignment - New Course Page',
+  description: 'Add new course to the database.',
+};
+
+// Optimize - Lazy Load the Heavy Client component only when needed.
