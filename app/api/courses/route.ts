f40e1c922468c48094f726d659ffceb70e9b07a2
TheCharismaticEnigma <avishkar2002@mail.com>
2023-11-07 15:40:23 +0530

    Backend: Create a route handler to add NEW course to DB

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,7 +2,7 @@
 
 import { connectToDatabase } from '@/dbConfig/dbConfig';
 import { Course } from '@/models/courseModel';
-import { CourseInterface } from '@/schemas/courseSchema';
+import { CourseInterface, CourseSchema } from '@/schemas/courseSchema';
 import { NextRequest, NextResponse } from 'next/server';
 
 connectToDatabase();
@@ -37,6 +37,50 @@ export async function GET(request: NextRequest) {
   }
 }
 
+// Edge Cases: Invalid body structure | Already existing title
 export async function POST(request: NextRequest) {
-  // Add a new course to the course list.
+  try {
+    const body: CourseInterface = await request.json();
+
+    const validation = CourseSchema.safeParse(body);
+
+    // Not a valid course structure/schema
+    if (!validation.success)
+      return NextResponse.json(
+        {
+          error:
+            'The provided structure for the course does not match the schema. Please check again. ',
+        },
+        { status: 400 }
+      );
+
+    // Check if a course w/ similar title exists or not
+    const preExistingCourse = await Course.findOne({ title: body.title });
+    if (preExistingCourse)
+      return NextResponse.json(
+        {
+          error: 'Course with similar title already exists.',
+        },
+        { status: 400 }
+      );
+
+    const newCourse = new Course(body);
+    const savedCourse = await newCourse.save();
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: 'New course added successfully.',
+        course: savedCourse,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: 'Something went wrong.',
+      },
+      { status: 500 }
+    );
+  }
 }
