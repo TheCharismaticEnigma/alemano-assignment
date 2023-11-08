21597827932aa16d21bb57475d3ffb477c7cb5a2
TheCharismaticEnigma <avishkar2002@mail.com>
2023-11-08 21:40:38 +0530

    Generate Dynamic Search Query Strings for Input

--- app/courses/(components)/InstructorSearchAction.tsx
+++ app/courses/(components)/InstructorSearchAction.tsx
@@ -1,15 +1,40 @@
 'use client';
 
 import {
-  Box,
   Input,
+  Button,
   InputGroup,
   InputLeftElement,
-  Select,
+  InputRightElement,
 } from '@chakra-ui/react';
-import { MagnifyingGlassIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { KeyboardEvent } from 'react';
 
 const InstructorSearchAction = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const setSearchParams: (e: KeyboardEvent<HTMLInputElement>) => void = (
+    event
+  ) => {
+    if (event.code !== 'Enter') return; // if key is not enter key
+    const criteria = 'instructor';
+    const input = event.target as HTMLInputElement;
+    const instructor = input.value.trim();
+    input.value = ''; // reset value
+
+    if (instructor.length === 0) return; // if empty string is sent
+
+    // Generate Search Query String Dynamically
+    const params = new URLSearchParams();
+    searchParams.forEach((value, key) => params.append(key, value));
+    params.set(criteria, instructor);
+
+    const queryString = params.size ? `?${params.toString()}` : '';
+    router.push(`/courses/${queryString}`);
+  };
+
   return (
     <InputGroup
       size={'lg'}
@@ -25,6 +50,7 @@ const InstructorSearchAction = () => {
         focusBorderColor="orange.200"
         borderWidth={'1px'}
         placeholder="Instructor Name..."
+        onKeyUp={setSearchParams}
       />
     </InputGroup>
   );
